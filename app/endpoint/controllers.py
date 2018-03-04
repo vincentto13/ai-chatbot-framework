30a7a89eebb83842bf62265ca4340bff952379e7
Stachu <stachu@vps378681.ovh.net>
2018-03-05 00:16:56 +0100

    chatbot_input -> chatbotInput

--- app/endpoint/controllers.py
+++ app/endpoint/controllers.py
@@ -100,7 +100,7 @@ def api():
         else:
             parameters = []
 
-        context["chatbot_input"] = requestJson.get("input")
+        context["chatbotInput"] = requestJson.get("input")
 
         if ((requestJson.get("complete") is None) or (
                 requestJson.get("complete") is True)):
@@ -200,7 +200,7 @@ def api():
                     context = {}
                     context["parameters"] = resultJson["extractedParameters"]
                     context["context"] = requestJson["context"]
-                    context["chatbot_input"] = requestJson.get("input")
+                    context["chatbotInput"] = requestJson.get("input")
                     try:
                         if story.apiTrigger:
                             isJson = False
